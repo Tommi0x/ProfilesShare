d58f10ec66a4ed3c7b7a81b9e0e0f03aa3cfd27c
Tommi0x <tommaso.toloni@icloud.com>
2023-11-27 20:01:49 +0100

    created a route

---
+++ app/api/content/route.tsx
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+
+//example data
+const posts = [
+    {
+      title: 'Lorem Ipsum',
+      slug: 'lorem-ipsum',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+    },
+  ];
+
+export async function GET(){
+    return NextResponse.json(posts);
+}
